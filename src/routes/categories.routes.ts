3967356ca6beb0120660ef35b452c9b6c1887fd5
Igor Serafim <igor.serafim@nolifretes.com.br>
2023-02-06 17:28:25 -0300

    start project

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express'
+import { v4 as uuidV4 } from 'uuid'
+import { Category } from '../module/Category'
+
+const categoriesRoutes = Router()
+
+const categories: Category[] = []
+
+categoriesRoutes.post('/', (req, res) => {
+  const { name, description } = req.body
+
+  const category = {
+    id: uuidV4(),
+    name,
+    description,
+    created_at: new Date(),
+  }
+
+  categories.push(category)
+
+  return res.status(201).send()
+})
+
+export { categoriesRoutes }
